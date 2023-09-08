ce9cdb4adfe766fcc1dd5e4c5eac6d7e915c2371
daz-3ux <daz-3ux@proton.me>
2023-09-08 18:47:49 +0800

    feat: update log system - add interface
    Signed-off-by: daz-3ux <daz-3ux@proton.me>

--- internal/pkg/log/log.go
+++ internal/pkg/log/log.go
@@ -13,11 +13,24 @@ import (
 	"time"
 )
 
+// Logger define dBlog's logger interface
+type Logger interface {
+	Debugw(msg string, keysAndValues ...interface{})
+	Infow(msg string, keysAndValues ...interface{})
+	Warnw(msg string, keysAndValues ...interface{})
+	Errorw(msg string, keysAndValues ...interface{})
+	Panicw(msg string, keysAndValues ...interface{})
+	Fatalw(msg string, keysAndValues ...interface{})
+	Sync()
+}
+
 // zapLogger wraps zap.Logger
 // the specific implementation of the Logger interface
 type zapLogger struct {
 	z *zap.Logger
 }
+
+var _ Logger = (*zapLogger)(nil)
 
 var (
 	mu sync.Mutex
@@ -76,4 +89,60 @@ func NewLogger(opts *Options) *zapLogger {
 	zap.RedirectStdLog(z)
 
 	return logger
+}
+
+func Sync() {
+	std.Sync()
+}
+
+func (l *zapLogger) Sync() {
+	_ = l.z.Sync()
+}
+
+func Debugw(msg string, keysAndValues ...interface{}) {
+	std.z.Sugar().Debugw(msg, keysAndValues...)
+}
+
+func (l *zapLogger) Debugw(msg string, keysAndValues ...interface{}) {
+	l.z.Sugar().Debugw(msg, keysAndValues...)
+}
+
+func Infow(msg string, keysAndValues ...interface{}) {
+	std.z.Sugar().Infow(msg, keysAndValues...)
+}
+
+func (l *zapLogger) Infow(msg string, keysAndValues ...interface{}) {
+	l.z.Sugar().Infow(msg, keysAndValues...)
+}
+
+func Warnw(msg string, keysAndValues ...interface{}) {
+	std.z.Sugar().Warnw(msg, keysAndValues...)
+}
+
+func (l *zapLogger) Warnw(msg string, keysAndValues ...interface{}) {
+	l.z.Sugar().Warnw(msg, keysAndValues...)
+}
+
+func Errorw(msg string, keysAndValues ...interface{}) {
+	std.z.Sugar().Errorw(msg, keysAndValues...)
+}
+
+func (l *zapLogger) Errorw(msg string, keysAndValues ...interface{}) {
+	l.z.Sugar().Errorw(msg, keysAndValues...)
+}
+
+func Panicw(msg string, keysAndValues ...interface{}) {
+	std.z.Sugar().Panicw(msg, keysAndValues...)
+}
+
+func (l *zapLogger) Panicw(msg string, keysAndValues ...interface{}) {
+	l.z.Sugar().Panicw(msg, keysAndValues...)
+}
+
+func Fatalw(msg string, keysAndValues ...interface{}) {
+	std.z.Sugar().Fatalw(msg, keysAndValues...)
+}
+
+func (l *zapLogger) Fatalw(msg string, keysAndValues ...interface{}) {
+	l.z.Sugar().Fatalw(msg, keysAndValues...)
 }
